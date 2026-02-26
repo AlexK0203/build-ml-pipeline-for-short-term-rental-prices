175e7745f4e98b7301781a3b14285bb7b448f02c
Alexander Kindle <alexander.a.kindle@accenture.com>
2026-02-26 18:31:32 +0100

    successful setup for getting date and cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,7 @@
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -16,13 +17,18 @@ def go(args):
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
@@ -31,27 +37,46 @@ if __name__ == "__main__":
 
 
     parser.add_argument(
-        "--parameter1", 
-        type= int, ## INSERT TYPE HERE: str, float or int
-        help= "test", ## INSERT DESCRIPTION HERE
+        "--input_artifact", 
+        type= str,
+        help= "artifact from previous step in W&B",
         required=True
     )
 
 
     parser.add_argument(
-        "--parameter2", 
-        type= int, ## INSERT TYPE HERE: str, float or int,
-        help= "test", ## INSERT DESCRIPTION HERE,
+        "--output_artifact", 
+        type= str,
+        help= "Name of the cleaned data set to upload",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type= str, ## INSERT TYPE HERE: str, float or int,
-        help= "test", ## INSERT DESCRIPTION HERE,
+        "--output_type", 
+        type= str,
+        help= "Type of the output",
         required=True
     )
 
+    parser.add_argument(
+        "--output_description", 
+        type= str,
+        help= "Description of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type= int,
+        help= "minimum price",
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type= int,
+        help= "maximum price",
+        required=True
+    )
 
     args = parser.parse_args()
 
